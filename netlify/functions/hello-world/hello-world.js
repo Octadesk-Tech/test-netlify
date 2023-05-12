23e87d0c404d475d4b13e4eadc0c76e68fb5aa2c
João Guilherme <joaaogui@gmail.com>
2023-05-12 18:16:06 -0300

    test

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -4,16 +4,25 @@ const handler = async (event) => {
   try {
     const body =JSON.parse(event.body)
     const catalog = JSON.parse(JSON.parse(body.catalog))
-    const items = catalog.items[0]  
+    const items = catalog.items
     const response = {
       "product_id":items.retailerId,
       "price":items.price.amount,
       "original_price":items.price.amount,
       "quantity":items.quantity
     }
+    let result = []
+    for(let item in items) {
+      result.push({
+        "product_id":item.retailerId,
+        "price":item.price.amount,
+        "original_price":item.price.amount,
+        "quantity":item.quantity
+      })
+    }
     return {
       statusCode: 200,
-      body: JSON.stringify(response),
+      body: JSON.stringify(result),
 
     }
   } catch (error) {
