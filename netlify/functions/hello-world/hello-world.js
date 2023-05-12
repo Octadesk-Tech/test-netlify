b894344c6d078c628f1b24131c21d1794d377cce
João Guilherme <joaaogui@gmail.com>
2023-05-12 16:45:48 -0300

    test

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -2,20 +2,23 @@
 
 const handler = async (event) => {
   try {
-    const data = JSON.parse(event).items[0]
-    const response = {
-      "product_id":data.retailerId,
-      "price":data.price.amount,
-      "original_price":data.price.amount,
-      "quantity":data.quantity  
-    }
+    conspole.log(event)
+    // const data = JSON.parse(event).items[0]
+    // const response = {
+    //   "product_id":data.retailerId,
+    //   "price":data.price.amount,
+    //   "original_price":data.price.amount,
+    //   "quantity":data.quantity  
+    // }
+    // console.log(response)
   
     return {
       statusCode: 200,
-      body: JSON.stringify(response),
+      body: typeof event,
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
 }
+handler()
 module.exports = { handler }
