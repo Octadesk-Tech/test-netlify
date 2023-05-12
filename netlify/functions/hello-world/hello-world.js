c5ae9a03992212369959852e1c8699510b660194
João Guilherme <joaaogui@gmail.com>
2023-05-12 16:40:06 -0300

    test

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -2,25 +2,20 @@
 
 const handler = async (event) => {
   try {
-    const data = JSON.parse(event).body
-    console.log(event)
-    // const response = {
-    //   "product_id":data.retailerId,
-    //   "price":data.price.amount,
-    //   "original_price":data.price.amount,
-    //   "quantity":data.quantity  
-    // }
+    const data = JSON.parse(event).items[0]
+    const response = {
+      "product_id":data.retailerId,
+      "price":data.price.amount,
+      "original_price":data.price.amount,
+      "quantity":data.quantity  
+    }
   
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify(response),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
 }
-
 module.exports = { handler }
