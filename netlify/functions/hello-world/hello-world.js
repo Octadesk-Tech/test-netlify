d6f469fac432318893660f34298fc641773ceba1
João Guilherme <joaaogui@gmail.com>
2023-05-15 15:47:02 -0300

    test

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -2,27 +2,32 @@
 const axios = require('axios');
 const handler = async (event) => {
   try {
-    // const body = JSON.parse(event.body)
-    // const order = JSON.parse(body.order)
-    // const catalog = JSON.parse(body.catalog)
-    // const items = catalog.items
-    // let result = []
-    // items.forEach(item => {
-    //   result.push({
-    //     "product_id":item.retailerId,
-    //     "price":item.price.amount,
-    //     "original_price":item.price.amount,
-    //     "quantity":item.quantity
-    //   })
-    // }) 
+    const body = JSON.parse(JSON.parse(event.body))
+    const order = body.Order
+    const token = body.access_token
+    const catalog = JSON.parse(body.catalog)
+    const items = catalog.items
+    let result = []
+    items.forEach(item => {
+      result.push({
+        "product_id":item.retailerId,
+        "price":item.price.amount,
+        "original_price":item.price.amount,
+        "quantity":item.quantity
+      })
+    }) 
 
-    // const trayURL = 'https://octadeskchat.commercesuite.com.br/web_api/orders?access_token=APP_ID-3913-STORE_ID-1156053-b0401b99e49b34e716410f181f8074b4efab46657ed6195ce5e141655ff23e15'
-    // const trayObject = {'Order': order, 'ProductsSold': result }
-    // const trayResult = axios.post(trayURL, trayObject)
-
+    const trayURL = `https://octadeskchat.commercesuite.com.br/web_api/orders?access_token=${token}`
+    const trayObject = {'Order': order, 'ProductsSold': result }
+    const trayResult = await axios({
+      method: 'post',
+      url: trayURL,
+      data: {...trayObject}
+    });
+    console.log(trayResult.data)
     return {
       statusCode: 200,
-      body: JSON.stringify(event),
+      body: JSON.stringify(trayResult.data),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
