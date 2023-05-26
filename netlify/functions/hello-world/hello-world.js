a48716469b67be1ca2d5f691b0ecc50c5087c8fc
João Guilherme <joaaogui@gmail.com>
2023-05-26 16:19:16 -0300

    teste

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,5 +1,5 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-import fetch from 'node-fetch';
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const handler = async (event) => {
   try {
