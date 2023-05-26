5abe5192efa885cbc78cf630a4bfb9709f2a8b5e
João Guilherme <joaaogui@gmail.com>
2023-05-26 16:16:34 -0300

    teste

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,4 +1,6 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+import fetch from 'node-fetch';
+
 const handler = async (event) => {
   try {
     // const body = JSON.parse(event.body);
