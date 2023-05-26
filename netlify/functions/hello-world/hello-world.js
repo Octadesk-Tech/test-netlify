4d8e18cc529cf886d43ea7eb5732eab690df5b8e
João Guilherme <joaaogui@gmail.com>
2023-05-26 16:08:01 -0300

    teste

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -17,45 +17,53 @@ const handler = async (event) => {
     //   });
     // });
 
-    const trayURL = `https://octadeskchat.commercesuite.com.br/web_api/orders?access_token=APP_ID-3913-STORE_ID-1156053-78fcbf79ec4a0062f5b26fe9b95d2057812c6296f7d1b6679a5c96dc2836023c`;
-    const trayObject = {
-      "Order":{
-        "point_sale":"Whastapp",
-        "shipment":"Retirar no local",
-        "Customer":{"type":"0",
-        "name":"Gabriel teste",
-        "cpf":"45062954800",
-        "email":"gabriel.miranda.rubio@gmail.com",
-        "phone":"+55 11 98471 4265",
-        "CustomerAddress":[{
-            "address":"Rua professor dario ribeiro",
-            "zip_code":"02559000",
-            "number":"937",
-            "complement":"",
-            "neighborhood":"Vila Prado",
-            "city":"São Paulo",
-            "state":"SP",
-            "country":"BRA",
-            "type": "1"
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({
+      "Order": {
+        "point_sale": "Whastapp",
+        "shipment": "Retirar no local",
+        "Customer": {
+          "type": "0",
+          "name": "Gabriel teste",
+          "cpf": "45062954800",
+          "email": "gabriel.miranda.rubio@gmail.com",
+          "phone": "+55 11 98471 4265",
+          "CustomerAddress": [
+            {
+              "address": "Rua professor dario ribeiro",
+              "zip_code": "02559000",
+              "number": "937",
+              "complement": "",
+              "neighborhood": "Vila Prado",
+              "city": "São Paulo",
+              "state": "SP",
+              "country": "BRA",
+              "type": "1"
             }
-        ]},
-        "ProductsSold":[{
-            "product_id":27,
-            "price":"150.00",
-            "original_price":"150.00",
-            "quantity":1  
+          ]
+        },
+        "ProductsSold": [
+          {
+            "product_id": 57,
+            "price": "150.00",
+            "original_price": "150.00",
+            "quantity": 1
           }
         ]
-      }    
-  };
-  console.log(trayObject)
-  let trayResult = await axios.post(
-      trayURL, JSON.stringify(trayObject),{
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    }
-    )
+      }
+    });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    let trayResult= await fetch("https://octadeskchat.commercesuite.com.br/web_api/orders?access_token=APP_ID-3913-STORE_ID-1156053-78fcbf79ec4a0062f5b26fe9b95d2057812c6296f7d1b6679a5c96dc2836023c", requestOptions)
+    trayResult = await trayResult.text()
+
 return {
   statusCode: 200,
   body: JSON.stringify(trayResult),
