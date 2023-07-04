3e03dcb7c87f9aa992c17696ef674f25bd8c7d32
João Guilherme <joaaogui@gmail.com>
2023-07-04 10:18:41 -0300

    test

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,11 +1,13 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const handler = async (event) => {
   try {
     const body = JSON.parse(event.body);
     const order = body.Order;
     const token = body.access_token;
+    const domainUrl = body.domainUrl;
     const catalog = JSON.parse(body.catalog);
     const items = catalog.items;
     let result = [];
@@ -19,27 +21,30 @@ const handler = async (event) => {
     });
 
     var raw = JSON.stringify({
-      "Order": {
-         ...order, 'ProductsSold': result }      
+      Order: {
+        ...order,
+        ProductsSold: result,
+      },
     });
-    
+
     var requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: raw,
-      redirect: 'follow'
+      redirect: "follow",
     };
-    let trayResult= await fetch(`https://octadeskchat.commercesuite.com.br/web_api/orders?access_token=${token}`, requestOptions)
-    trayResult = await trayResult.text()
+    let trayResult = await fetch(
+      `https://${domainUrl}.commercesuite.com.br/web_api/orders?access_token=${token}`,
+      requestOptions
+    );
+    trayResult = await trayResult.text();
 
-return {
-  statusCode: 200,
-  body: JSON.stringify(JSON.parse(trayResult)),
-};
-    
-
+    return {
+      statusCode: 200,
+      body: JSON.stringify(JSON.parse(trayResult)),
+    };
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
   }
